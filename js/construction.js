87cbc5e7f53372dfa11885c0f3839a54750907df
dhavaldhanani <dhananidhaval72@gmail.com>
2023-10-03 09:51:54 +0530

    first

---
+++ js/construction.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+ 
+  $(".owl-carousel").owlCarousel({
+    items:1,
+    margin:20,
+    // loop:true,
+    // center:true,
+    mouseDrag:false,
+    merge:true,
+    // stagePadding:100,
+    // rewind:true,
+    // slideBy:3,
+    startPosition:0,
+    rewind:true,
+    // slideTransition:'cubic-bezier(.36,1.56,.7,.05)',
+    // dots:false,
+    // dotsEach:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    // autoplayHoverPause:true,
+    // autoplaySpeed:7000,
+  });
+});   
